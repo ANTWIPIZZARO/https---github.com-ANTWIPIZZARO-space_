cb51c777071656c3336499782c4b88a0275f4ced
clinton <pizzarogh003@gmail.com>
2024-07-24 06:59:24 -0700

    update

---
+++ space/src/Components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-slate-800">
+      <footer>
+        <div>
+          <ul>
+            <li>Our Product</li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ space/src/Components/Navbar/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="w-full  bg-slate-200 flex gap-40 ">
+        <div className="font-bold text-4xl ">Pizzaro</div>
+        <ul className="flex items-center justify-center gap-10">
+          <li>Home</li>
+          <li>Categories </li>
+          <li>Trending</li>
+          <li>Contact Us </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
